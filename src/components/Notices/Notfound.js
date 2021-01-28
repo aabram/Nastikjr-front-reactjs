4ffaafbd8151d8fe2734f1c9442816c3d6fa5f3a
Sven Vahar <sven.vahar@gmail.com>
2021-01-28 11:26:38 +0200

    Added propTypes, msg for searching in reverse

--- src/components/Notices/Notfound.js
+++ src/components/Notices/Notfound.js
@@ -1,12 +1,30 @@
-import React from 'react';
-import {Text} from "@chakra-ui/core";
+import React from "react";
+import { Text } from "@chakra-ui/core";
+import "../../css/App.css";
+import { string } from "prop-types";
 
-function Notfound() {
+function Notfound(props) {
+  const direction =
+    props.lang === "en"
+      ? ["Inglise → eesti", "Eesti → inglise", "/et/" + props.word]
+      : ["Eesti → inglise", "Inglise → eesti", "/en/" + props.word];
+
   return (
-    <React.Fragment>
-      <Text my={2}>Ei leidnud midagi.</Text>
-    </React.Fragment>
+    <Text my={2}>
+      Ei leidnud midagi.
+      <br />
+      Otsisid suunal <em>{direction[0]}</em>, kas tahad proovida otsida suunal{" "}
+      <a className="link" href={direction[2]}>
+        {direction[1]}
+      </a>
+      ?
+    </Text>
   );
 }
 
 export default Notfound;
+
+Notfound.propTypes = {
+  lang: string.isRequired,
+  word: string.isRequired,
+};
